d1d40e956dd3589e93ce268ef71e57136d2ed90b
Louis Heal <louisheal@live.co.uk>
2024-08-08 21:12:38 +0100

    use primitive objectid for clubid and userid

--- dao/club.go
+++ dao/club.go
@@ -1,6 +1,8 @@
 package dao
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Club struct {
-	ID   string `json:"id" bson:"_id"`
-	Name string `json:"name" bson:"name"`
+	ID   primitive.ObjectID `json:"id" bson:"_id"`
+	Name string             `json:"name" bson:"name"`
 }
